e69eaeaa5a1cda710f50c1399e84d1297ee006fd
Jeff Hui <huij@rpi.edu>
2011-09-16 18:44:36 -0400

    Forgot a comma, making django complain.

--- settings.py
+++ settings.py
@@ -87,7 +87,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    relative('static', 'global')
+    relative('static', 'global'),
 )
 
 # List of finder classes that know how to find static files in
