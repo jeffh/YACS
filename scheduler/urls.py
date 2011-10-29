7fbe14f15ae96c9243c10d78977fecd43dc64293
Jeff Hui <huij@rpi.edu>
2011-10-29 16:30:56 -0400

    Changed schedules urls. Removed uncached and server-side template variant.

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -3,9 +3,9 @@ from yacs.scheduler import views
 
 urlpatterns = patterns('',
     # selecting courses
-    url(r'^(?P<year>[1-9]\d*)/(?P<month>[1-9]\d*)/schedules/$', views.force_compute_schedules, name='schedules'),
-    url(r'^(?P<year>[1-9]\d*)/(?P<month>[1-9]\d*)/new-schedules/ajax/$', views.json_compute_schedules_via_cache, name='ajax-schedules'),
-    url(r'^(?P<year>[1-9]\d*)/(?P<month>[1-9]\d*)/cached-schedules/$', views.compute_schedules_via_cache, name='schedules'),
-    url(r'^(?P<year>[1-9]\d*)/(?P<month>[1-9]\d*)/new-schedules/$', views.schedules_bootloader, name='schedules'),
+    #url(r'^(?P<year>[1-9]\d*)/(?P<month>[1-9]\d*)/schedules/$', views.force_compute_schedules, name='schedules'),
+    url(r'^(?P<year>[1-9]\d*)/(?P<month>[1-9]\d*)/schedules/ajax/$', views.json_compute_schedules_via_cache, name='ajax-schedules'),
+    #url(r'^(?P<year>[1-9]\d*)/(?P<month>[1-9]\d*)/cached-schedules/$', views.compute_schedules_via_cache, name='schedules'),
+    url(r'^(?P<year>[1-9]\d*)/(?P<month>[1-9]\d*)/schedules/$', views.schedules_bootloader, name='schedules'),
 
 )
