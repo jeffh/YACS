381f782e68c899f4319a6104e3749a824948e09a
Jeff Hui <huij@rpi.edu>
2011-12-21 17:59:29 -0500

    Updated model.js for potential reintroduction (probably not).

--- static/global/js/models.js
+++ static/global/js/models.js
@@ -1,102 +1,169 @@
-(function(Backbone, _, undefined){
+(function(Backbone, _, window, undefined){
 
-function api(){
-    return '/api/' + Array.prototype.join.call(arguments, '/') + '/';
-}
-function includeURLinToJSON(){
-    var data = Backbone.Model.prototype.toJSON.call(this);
-    data.url = this.url().substr('/api'.length);
-    var self = this;
-    _.each(this.jsonifyFields || [], function(name){
+  function readOnlySync(method, model, options){
+    if (_.include(['create', 'update', 'delete'], method))
+      return;
+    return Backbone.sync(method, model, options);
+  }
+
+  function api(){
+    return '/api/v3/' + Array.prototype.join.call(arguments, '/') + '/';
+  }
+  function parseJSONReponse(json){
+    if (json.status === 'OK'){
+      return json.payload;
+    } else {
+      console.log('JSON request failed: ', json);
+    }
+  }
+
+  var ModelBase = Backbone.Model.extend({
+    initialize: function(attributes){
+      var self = this;
+      _.each(attributes, function(value, name){
+        var klass = window[(self.collectionFields || {})[name]];
+        if (klass){
+          obj = {};
+          obj[name] = self._collectionize(value, klass);
+          self.set(obj, {slient: true});
+        }
+      });
+    },
+    _collectionize: function(json, collectionClass){
+      var collection = new collectionClass();
+      _.each(json, function(item){
+        collection.create(item);
+      });
+      return collection;
+    },
+    collectionFields: null,
+    sync: readOnlySync,
+    jsonifyFields: [],
+    toJSON: function(){
+      var data = Backbone.Model.prototype.toJSON.call(this);
+      data.url = this.url().substr('/api/v3'.length);
+      var self = this;
+      _.each(this.jsonifyFields || [], function(name){
         data[name] = self.get(name).toJSON();
-    });
-    return data;
-}
-
-var Semester = Backbone.Model.extend({
-    url: function(){
-        return api(this.get('year'), this.get('month'));
+      });
+      return data;
     },
-    toJSON: includeURLinToJSON
-});
+    parse: parseJSONReponse
+  });
 
-var Department = Backbone.Model.extend({
-    url: function(){
-        var sem = this.get('semester');
-        return api(sem.get('year'), sem.get('month'), this.get('code'));
+  var CollectionBase = Backbone.Collection.extend({
+    semesterID: null,
+    getSemesterQueryString: function(){
+      if (!this.get('semesterID')){
+        return '';
+      }
+      return 'semester=' + this.get('semesterID');
     },
-    toJSON: includeURLinToJSON
-});
+    parse: parseJSONReponse
+  });
 
-var Course = Backbone.Model.extend({
+  var Semester = ModelBase.extend({
+    url: function(){
+      return api('semesters', this.id);
+    }
+  });
+
+  var Department = ModelBase.extend({
+    url: function(){
+      var sem = this.get('semester');
+      return api('departments', this.get('code'));
+    }
+  });
+
+  var Course = ModelBase.extend({
     defaults: {
-        marked: false,
-        sections: null
+      marked: false,
+      sections: null
     },
     url: function(){
-        var sem = this.get('semester');
-        return api(sem.get('year'), sem.get('month'), this.get('code'), this.get('number'));
+      var sem = this.get('semester');
+      return api('courses', this.id);
     },
     jsonifyFields: ['semester'],
-    toJSON: includeURLinToJSON,
     mark: function(){
-        _.each(this.get('sections'), function(section){
-            section.mark();
-        });
-        this.set({marked: true});
-        console.log('course marked ' + this.get('name'));
+      _.each(this.get('sections'), function(section){
+        section.mark();
+      });
+      this.set({marked: true});
+      console.log('course marked ' + this.get('name'));
     },
     unmark: function(){
-        _.each(this.get('sections'), function(section){
-            section.unmark();
-        });
-        this.set({marked: false});
-        console.log('course unmarked ' + this.get('name'));
+      _.each(this.get('sections'), function(section){
+        section.unmark();
+      });
+      this.set({marked: false});
+      console.log('course unmarked ' + this.get('name'));
     }
-});
+  });
 
-var Section = Backbone.Model.extend({
+  var Section = ModelBase.extend({
     defaults: {
-        marked: false
+      marked: false
     },
+    idAttribute: 'crn',
+    collectionFields: {periods: 'PeriodList'},
     jsonifyFields: ['course', 'semester'],
     url: function(){
-        var sem = this.get('semester');
-        return api(sem.get('year'), sem.get('month'), this.get('code'), this.get('number'), 'crn-' + this.get('crn'));
+      var sem = this.get('semester');
+      return api('sections', this.get('crn'));
     },
-    toJSON: includeURLinToJSON,
     mark: function(){
-        this.set({marked: true});
-        console.log('section marked ' + this.get('crn'));
+      this.set({marked: true});
+      console.log('section marked ' + this.get('crn'));
     },
     unmark: function(){
-        this.set({marked: false});
-        console.log('section unmarked ' + this.get('crn'));
+      this.set({marked: false});
+      console.log('section unmarked ' + this.get('crn'));
     }
-});
+  });
 
-var Period = Backbone.Model.extend({
+  var Period = ModelBase.extend({
     url: function(){
-        var semester = this.section.semester;
-        return api('periods', this.id);
-    },
-    toJSON: includeURLinToJSON
-});
+      return api('periods', this.id);
+    }
+  });
 
-var Semesters = Backbone.Collection.extend({
+  var Semesters = ModelBase.extend({
     model: Semester,
     comparator: function(semester){
-        return semester.get('year') * 12 + semester.get('month');
+      return semester.get('year') * 12 + semester.get('month');
     },
-    toJSON: includeURLinToJSON
-});
-var Departments = Backbone.Collection.extend({ model: Department });
-var Courses = Backbone.Collection.extend({ model: Course });
-var Sections = Backbone.Collection.extend({ model: Section });
-var Periods = Backbone.Collection.extend({ model: Period });
+    url: function(){
+      return api('semesters');
+    }
+  });
+  var Departments = CollectionBase.extend({
+    model: Department,
+    url: function(){
+      return api('departments') + '?' + this.getSemesterQueryString();
+    }
+  });
+  var Courses = CollectionBase.extend({
+    model: Course,
+    url: function(){
+      return api('courses') + '?' + this.getSemesterQueryString();
+    }
+  });
+  var Sections = CollectionBase.extend({
+    model: Section,
+    url: function(){
+      return api('sections') + '?' + this.getSemesterQueryString();
+    }
+  });
+  var Periods = CollectionBase.extend({
+    model: Period,
+    url: function(){
+      return api('periods') + '?' + this.getSemesterQueryString();
+    }
+  });
 
-// export
-_.extend(window, {
+  // export
+  _.extend(window, {
     Semester: Semester,
     Department: Department,
     Course: Course,
@@ -107,6 +174,6 @@ _.extend(window, {
     CourseList: Courses,
     SectionList: Sections,
     PeriodList: Periods
-})
+  })
 
-})(Backbone, _);
+})(Backbone, _, window);
