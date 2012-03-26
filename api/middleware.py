da3765851d5cc5fabd3fc0b5d031d4f884655c4c
Jeff Hui <contrib@jeffhui.net>
2012-03-26 19:11:45 -0400

    Refactored to shorter middleware names.

--- api/middleware.py
+++ api/middleware.py
@@ -1,11 +1,11 @@
 import re
 
-import django.contrib.sessions.middleware
-import django.contrib.auth.middleware
-import django.contrib.messages.middleware
+from django.contrib.sessions.middleware import SessionMiddleware as OriginalSessionMiddleware
+from django.contrib.auth.middleware import AuthenticationMiddleware as OriginalAuthenticationMiddleware
+from django.contrib.messages.middleware import MessageMiddleware as OriginalMessageMiddleware
 from django.conf import settings
 
-import debug_toolbar.middleware
+from  debug_toolbar.middleware import DebugToolbarMiddleware as OriginalDebugToolbarMiddleware
 
 
 EXCLUDED = [re.compile(url) for url in getattr(settings, 'SESSION_EXCLUDED_URLS', [])]
@@ -25,16 +25,18 @@ class WrapRequestResponse(object):
     def process_response(self, request, response):
         if is_excluded(request.path_info):
             return response
-        return getattr(super(WrapRequestResponse, self), 'process_response', lambda req, rep: response)(request, response)
+        return getattr(super(WrapRequestResponse, self),
+                'process_response',
+                lambda req, rep: response)(request, response)
 
-class SessionMiddleware(WrapRequestResponse, django.contrib.sessions.middleware.SessionMiddleware):
+class SessionMiddleware(WrapRequestResponse, OriginalSessionMiddleware):
     pass
 
-class AuthenticationMiddleware(WrapRequestResponse, django.contrib.auth.middleware.AuthenticationMiddleware):
+class AuthenticationMiddleware(WrapRequestResponse, OriginalAuthenticationMiddleware):
     pass
 
-class MessageMiddleware(WrapRequestResponse, django.contrib.messages.middleware.MessageMiddleware):
+class MessageMiddleware(WrapRequestResponse, OriginalMessageMiddleware):
     pass
 
-class DebugToolbarMiddleware(WrapRequestResponse, debug_toolbar.middleware.DebugToolbarMiddleware):
+class DebugToolbarMiddleware(WrapRequestResponse, OriginalDebugToolbarMiddleware):
     pass
