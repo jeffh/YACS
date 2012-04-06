728e235ace11f5d1dd798e438ea65bde8a41bd6e
David Hedin <lucky.hedin13@mgail.com>
2012-04-05 20:44:14 -0400

    Revert "Revert "corrected database settings merge again""
    This reverts commit e7766ce068eabea30c81ba699c77ed2fe488d69d.

--- yacs/settings/development.py
+++ yacs/settings/development.py
@@ -20,9 +20,9 @@ with settings as s:
     s.DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'mydata',
-            'USER': 'postgreuser',
-            'PASSWORD': 'postgre',
+            'NAME': 'yacs',
+            'USER': 'timetable',
+            'PASSWORD': 'thereisn0sp00n',
             'HOST': 'localhost',
             'PORT': '',
             'OPTIONS': {
