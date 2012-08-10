0681b2b321087e0725e3c802997719edf619287b
Jeff Hui <jeff@jeffhui.net>
2012-08-09 23:13:43 -0700

    Fixed visualizations to work. Removed staff requirement on visuals.

--- courses_viz/urls.py
+++ courses_viz/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 from courses_viz import views
 
 urlpatterns = patterns('',
+    url(r'^$', views.DefaultRedirect.as_view()),
     url(r'^bubbles/$', views.render_template, {'template': 'bubble.html'}, name='bubble'),
     url(r'^timelines/$', views.render_template, {'template': 'timelines.html'}, name='timelines'),
     url(r'^treemap/$', views.render_template, {'template': 'treemap.html'}, name='treemap'),
--- courses_viz/views.py
+++ courses_viz/views.py
@@ -1,13 +1,19 @@
+from django.views.generic import RedirectView
+from django.core.urlresolvers import reverse
+
 from courses.views.decorators import Renderer, staff_required
 
 
 render = Renderer(template_prefix='courses_viz/')
 
 
-@staff_required
 @render()
 def render_template(request, template):
     return {
         'template_name': template,
         'context': {},
     }
+
+class DefaultRedirect(RedirectView):
+    def get_redirect_url(self):
+        return reverse('courses_viz:bubble')
