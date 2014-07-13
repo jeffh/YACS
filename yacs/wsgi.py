09981eb1525f195c1d5f086d70a1f8444cba58fe
Jeff Hui <jeff@jeffhui.net>
2014-07-13 03:22:24 -0700

    Added Cling

--- yacs/wsgi.py
+++ yacs/wsgi.py
@@ -12,3 +12,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yacs.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+from dj_static import Cling
+application = Cling(application)
