4325b6463b00cd8515cca9b062962603d5768238
Jeff Hui <jeff@jeffhui.net>
2014-07-13 16:55:23 -0700

    Added detection of buildpack to import environment vars

--- yacs/settings/__init__.py
+++ yacs/settings/__init__.py
@@ -3,6 +3,24 @@ import sys
 
 from django.core.exceptions import ImproperlyConfigured
 
+if 'ENV_DIR' in os.environ:
+    print 'Detected buildpack environment. Loading environment vars from', os.environ['ENV_DIR']
+    keys = (
+        'YACS_ENV',
+        'YACS_SECRET_KEY',
+        'YACS_DATABASE_URL',
+        'YACS_DISABLE_FILE_SYSTEM_LOGGING',
+        'DATABASE_URL',
+        'MEMCACHIER_SERVERS',
+        'MEMCACHIER_USERNAMAE',
+        'MEMCACHIER_PASSWORD',
+        'MEMCACHE_SERVERS',
+        'MEMCACHE_USERNAMAE',
+        'MEMCACHE_PASSWORD',
+    )
+    for key in keys:
+        os.environ[key] = open(os.path.join(os.environ['ENV_DIR'], key), 'r').read()
+
 
 is_running_tests = 'test' in sys.argv
 if is_running_tests:
