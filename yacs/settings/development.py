e11141cfe43ab5c2ad12b8556a8bef51bd88200e
Lou, Yufan <loganlyf@gmail.com>
2014-12-05 02:29:33 -0500

    Add DATABASES to django settings under development.

--- yacs/settings/development.py
+++ yacs/settings/development.py
@@ -9,3 +9,14 @@ CACHES = {
         'VERSION': CACHE_VERSION,
     }
 }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'yacsdb',
+        'USER': 'yacs',
+        'PASSWORD': 'yacs',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
